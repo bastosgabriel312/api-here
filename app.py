cfad6dbcf380be04e162ea63e071e24510da42cb
bastosgabriel312 <bastosgabriel312@gmail.com>
2022-04-30 21:44:14 -0300

    Alterações no mapa

--- app.py
+++ app.py
@@ -5,24 +5,26 @@ app = Flask(__name__)
 
 
 def localizar(palavraChave):
-    loc = Nominatim(user_agent="GetLoc") 
-    getLoc = loc.geocode(palavraChave) 
-    print(getLoc.address) 
-    print("Latitude = ", getLoc.latitude, "\n") 
-    print("Longitude = ", getLoc.longitude) 
-    return getLoc
+    try:
+        loc = Nominatim(user_agent="GetLoc") 
+        getLoc = loc.geocode(palavraChave) 
+        print(getLoc.address)
+        return getLoc
+    except:
+        loc = Nominatim(user_agent="GetLoc") 
+        getLoc = loc.geocode("Avenida Rudge 315 Bom Retiro São Paulo") 
+        return getLoc
     
 
 
 
 @app.route('/', methods=['GET','POST'])
-def map_func():
+def mapa():
     if request.method == 'POST':
         enderecoForm = request.form['endereco']
         loc = localizar(enderecoForm)
         return render_template('index.html', localizacao = loc)
-
-    return render_template('index.html', localizacao = localizar("São Paulo"))
+    return render_template('index.html', localizacao = localizar("Avenida Rudge 315 Bom Retiro São Paulo"))
 
 if __name__ == '__main__':
     app.run(debug = True)    
